06046855f436fb1f0b7738cb5c57765ab3bcea30
Taylor Kingsbury <taylorkingsbury@gmail.com>
2019-04-06 18:42:37 -0400

    adding list of bears

---
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -0,0 +1,10 @@
+import { LightningElement, wire } from 'lwc';
+import ursusResources from '@salesforce/resourceUrl/ursus_park';
+/** BearController.getAllBears() Apex method */
+import getAllBears from '@salesforce/apex/BearController.getAllBears';
+export default class BearList extends LightningElement {
+	@wire(getAllBears) bears;
+	appResources = {
+		bearSilhouette: ursusResources +'/img/standing-bear-silhouette.png',
+	};
+}
