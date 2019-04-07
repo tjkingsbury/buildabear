7d069768c1ebfafb71ef48a182cb4b0e85270cda
Taylor Kingsbury <taylorkingsbury@gmail.com>
2019-04-06 21:25:11 -0400

    add bear map

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,14 +1,24 @@
-import { LightningElement, track, wire } from 'lwc';
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';
+import { loadStyle } from 'lightning/platformResourceLoader';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
+import { LightningElement, track, wire } from 'lwc';
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
-export default class BearList extends LightningElement {
+export default class BearListNav extends NavigationMixin(LightningElement) {
 	@track searchTerm = '';
+	@track bears;
+	@wire(CurrentPageReference) pageRef;
 	@wire(searchBears, {searchTerm: '$searchTerm'})
-	bears;
-	appResources = {
-		bearSilhouette: ursusResources +'/img/standing-bear-silhouette.png',
-	};
+	loadBears(result) {
+		this.bears = result;
+		if (result.data) {
+			fireEvent(this.pageRef, 'bearListUpdate', result.data);
+		}
+	}
+	connectedCallback() {
+		loadStyle(this, ursusResources + '/style.css');
+	}
 	handleSearchTermChange(event) {
 		// Debouncing this method: do not update the reactive property as
 		// long as this function is being called within a delay of 300 ms.
@@ -22,5 +32,16 @@ export default class BearList extends LightningElement {
 	}
 	get hasResults() {
 		return (this.bears.data.length > 0);
+	}
+	handleBearView(event) {
+		// Navigate to bear record page
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: event.target.bear.Id,
+				objectApiName: 'Bear__c',
+				actionName: 'view',
+			},
+		});
 	}
 }
